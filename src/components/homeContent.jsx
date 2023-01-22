95cc9bc53944ccf0c00e93e37059a59915cd6a66
towel-cool <daminecraft5@gmail.com>
2023-01-21 23:16:25 -0500

    changed some styling

--- src/components/homeContent.jsx
+++ src/components/homeContent.jsx
@@ -7,8 +7,8 @@ class HomeContent extends Component {
             <div className="flex flex-row justify-center">
                 <Link 
                 to='/hlGame'
-                className="rounded-md border-4 text-white border-orange-500 duration-150 text-6xl
-                hover:scale-110 hover:bg-orange-500 hover:text-stone-900"
+                className="rounded-md border-4 text-white border-purple-500 duration-150 text-6xl
+                hover:scale-110 hover:bg-purple-500 hover:text-stone-900"
                 >Play</Link>
             </div>
         );
